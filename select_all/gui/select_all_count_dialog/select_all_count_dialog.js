b01ef9e42dc886fc698ac513a692942623f51b5f
Filippo Dibenedetto <filippo.dibenedetto@unlogged.it>
2026-09-01 20:28:23 +0200

    add improved modify selection

--- select_all/gui/select_all_count_dialog/select_all_count_dialog.js
+++ select_all/gui/select_all_count_dialog/select_all_count_dialog.js
@@ -5,9 +5,29 @@ function select_all_count_selects()
     Engine.GetGUIObjectByName("select_all_count_dialog").hidden = true
     let size = Number(Engine.GetGUIObjectByName("select_all_count_label").caption)
     let selection = g_Selection.toList()
-    size = Math.min(size, selection.length)
+    let out = [];
+    let idle_flg = Engine.GetGUIObjectByName("idle_chk").checked;
+    let food_flg = Engine.GetGUIObjectByName("food_chk").checked;
+    let wood_flg = Engine.GetGUIObjectByName("wood_chk").checked;
+    let stone_flg = Engine.GetGUIObjectByName("stone_chk").checked;
+    let metal_flg = Engine.GetGUIObjectByName("metal_chk").checked;
+
+    if (idle_flg || food_flg || wood_flg || stone_flg || metal_flg) {
+        if (idle_flg) out.push(...idle(selection));  
+        if (food_flg) out.push(...gatherers(selection, "food"));  
+        if (wood_flg) out.push(...gatherers(selection, "wood"));  
+        if (stone_flg) out.push(...gatherers(selection, "stone"));  
+        if (metal_flg) out.push(...gatherers(selection, "metal"));  
+    }
+    else {
+        out = selection;
+    }
+    if (!Engine.GetGUIObjectByName("all_chk").checked) {
+        size = Math.min(size, out.length);
+        out = out.slice(0, size);
+    }
     g_Selection.reset();
-    g_Selection.addList(selection.slice(0, size))
+    g_Selection.addList(out);
 }
 function select_all_count_change(value)
 {
